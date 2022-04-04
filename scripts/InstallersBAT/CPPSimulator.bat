114a7f974b6bcf609aef9e274269d4e5c4c75494
fgclue <schefferdaudtfelipe@gmail.com>
2022-04-04 14:01:02 -0300

    CPPSim Installer

--- scripts/InstallersBAT/CPPSimulator.bat
+++ scripts/InstallersBAT/CPPSimulator.bat
@@ -9,13 +9,13 @@ if "%sure%" == "no" goto no
 
 :yes
 echo Where to store C++ Simulator?
-echo (e.g. C:\Directory\AliasCreator\)
+echo (e.g. C:\Directory\CPPSim\)
 set /p stored="> "
 if not exist "%stored%" mkdir %stored% > NUL
 cd /d %stored% 
-wget https://raw.githubusercontent.com/fgclue/repository-of-apps/main/windows/aliascreator.bat
-echo true > firsttime.acd
-echo Succefully Installed Alias Creator.
+wget https://raw.githubusercontent.com/fgclue/repository-of-apps/main/windows/CPPSim/CPPSim/cppsim.bat
+echo true > firstime.cpsd
+echo Succefully Installed C++ Simulator.
 pause
 exit
 
