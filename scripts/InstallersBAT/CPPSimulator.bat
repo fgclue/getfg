e069cab11b9d2079dfe6db3325e154b1f23ba00e
fgclue <schefferdaudtfelipe@gmail.com>
2022-04-04 13:51:31 -0300

    hello

---
+++ scripts/InstallersBAT/CPPSimulator.bat
@@ -0,0 +1,23 @@
+@echo off
+echo getfg Installer
+echo.
+echo Are you sure you wanna install C++ Simualator v1.0.0?
+set /p sure="> "
+
+if "%sure%" == "yes" goto yes
+if "%sure%" == "no" goto no
+
+:yes
+echo Where to store C++ Simulator?
+echo (e.g. C:\Directory\AliasCreator\)
+set /p stored="> "
+if not exist "C:\clueapps\CPPSim" mkdir C:\clueapps\CPPSim > NUL
+cd /d %stored% 
+wget https://raw.githubusercontent.com/fgclue/repository-of-apps/main/windows/aliascreator.bat
+echo true > firsttime.acd
+echo Succefully Installed Alias Creator.
+pause
+exit
+
+:no
+exit
