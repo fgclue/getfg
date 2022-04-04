a7fc71b57a98ec12a6ad3827866a7b0521c9357b
fgclue <schefferdaudtfelipe@gmail.com>
2022-04-04 14:06:29 -0300

    CPPSimulator Installer (Installer Added)

--- scripts/InstallersBAT/CPPSimulator.bat
+++ scripts/InstallersBAT/CPPSimulator.bat
@@ -12,7 +12,7 @@ echo Where to store C++ Simulator?
 echo (e.g. C:\Directory\CPPSim\)
 set /p stored="> "
 if not exist "%stored%" mkdir %stored% > NUL
-cd /d %stored%  
+cd /d %stored% 
 wget https://raw.githubusercontent.com/fgclue/repository-of-apps/main/windows/CPPSim/CPPSim/cppsim.bat
 echo true > firstime.cpsd
 echo Succefully Installed C++ Simulator.
